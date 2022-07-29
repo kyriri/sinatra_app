441c6f93ac70b2f7913afa0f35cf69bdf82b5620
kyriri <31296669+kyriri@users.noreply.github.com>
2022-07-29 18:15:34 -0300

    Persist Patients on the DB

--- app/information_updater.rb
+++ app/information_updater.rb
@@ -6,4 +6,29 @@ class InformationUpdater
     CSV.read(path, col_sep: ';')
   end
 
+  def self.create_patients(data)
+    indexes = {
+      patient_cpf: data[0].index('cpf'),
+      patient_name: data[0].index('nome paciente'),
+      patient_email: data[0].index('email paciente'),
+      patient_birth_date: data[0].index('data nascimento paciente'),
+      patient_address_street: data[0].index('endereço/rua paciente'),
+      patient_address_city: data[0].index('cidade paciente'),
+      patient_address_state: data[0].index('estado patiente'),
+    }
+
+    data.shift # removes headers
+
+    data.each do |line|
+
+      if Patient.where(name: line[indexes[:patient_name]]).empty?
+        Patient.create(
+          name: line[indexes[:patient_name]],
+          cpf: line[indexes[:patient_cpf]],
+          email: line[indexes[:patient_email]],
+          birth_date: line[indexes[:patient_birth_date]],
+        )
+      end
+    end
+  end
 end
--- spec/information_updater_spec.rb
+++ spec/information_updater_spec.rb
@@ -12,4 +12,25 @@ RSpec.describe InformationUpdater do
       expect(response[0]).to include 'email paciente'
     end
   end
+
+  context '.create_patients' do
+    it 'persists patients to db' do
+      data = [
+        ["cpf", "nome paciente", "email paciente", "data nascimento paciente", "endereço/rua paciente", "cidade paciente", "estado patiente"], 
+        ["018.581.237-63", "Ana Beatriz Rios", "hobert_marquardt@kulas.biz", "1989-09-26", "6425 Rua Emilly Nogueira", "Carnaubal", "Distrito Federal"], 
+        ["018.581.237-63", "Ana Beatriz Rios", "hobert_marquardt@kulas.biz", "1989-09-26", "6425 Rua Emilly Nogueira", "Carnaubal", "Distrito Federal"], 
+        ["037.787.232-60", "João Felipe Louzada", "clifton_hyatt@koss.biz", "1985-01-22", "s/n Viela Theo Modesto", "Cachoeira dos Índios", "Tocantins"],
+      ]
+      
+      InformationUpdater.create_patients(data)
+
+      expect(Patient.all.size).to be 2
+    end
+
+    xit 'does not duplicate patients' do
+    end
+
+    xit 'works even if the array contains more data' do
+    end
+  end
 end
