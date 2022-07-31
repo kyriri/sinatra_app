009025732a336030f2530e406016cc5ded3b01f0
kyriri <31296669+kyriri@users.noreply.github.com>
2022-07-31 18:30:14 -0300

    Test uniqueness of a Physician's full CRM

---
+++ spec/models/physician_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe Physician do
+  let(:app) { App.new }
+
+  context 'CRM registration' do
+    it 'allows repetition of CRM state' do
+      create(:physician, crm_state: 'CE', crm_number: '443')
+      
+      create(:physician, crm_state: 'CE', crm_number: '1021') 
+
+      expect(Physician.last.crm_state).to eq 'CE'
+      expect(Physician.last.crm_number).to eq '1021'
+    end
+
+    it 'allows repetition of CRM number' do
+      create(:physician, crm_state: 'CE', crm_number: '443')
+      
+      create(:physician, crm_state: 'AC', crm_number: '443') 
+
+      expect(Physician.last.crm_state).to eq 'AC'
+      expect(Physician.last.crm_number).to eq '443'
+    end
+
+    it 'blocks repetition of full CRM (state + number)' do
+      create(:physician, crm_state: 'CE', crm_number: '1021')
+      
+      expect { create(:physician, crm_state: 'CE', crm_number: '1021') }
+        .to raise_error ActiveRecord::RecordNotUnique
+    end
+
+    xit 'should be case-insensitive' do
+      create(:physician, crm_state: 'CE', crm_number: '10m21')
+      
+      expect { create(:physician, crm_state: 'ce', crm_number: '10M21') }
+        .to raise_error ActiveRecord::RecordNotUnique
+    end
+  end
+end
