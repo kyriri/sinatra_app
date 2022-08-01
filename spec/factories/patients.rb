fc9b796f42e1f49f7d5dd883943f49a847ab34d3
kyriri <31296669+kyriri@users.noreply.github.com>
2022-07-31 23:41:45 -0300

    Fix CPF generator regarding leading zeros

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :patient do
-    cpf { "#{rand(999)}.#{rand(999)}.#{rand(999)}-#{rand(99)}" }
+    cpf { (1..11).map { rand(9).to_s }.join.insert(6, '.').insert(3, '.').insert(-3, '-') }
   end
 end
