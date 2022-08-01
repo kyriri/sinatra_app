6c7332463040f8ba096dec289a797d292d698625
kyriri <31296669+kyriri@users.noreply.github.com>
2022-07-31 21:52:48 -0300

    Develop Patient and Test Report factories

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
   factory :patient do
+    cpf { "#{rand(999)}.#{rand(999)}.#{rand(999)}-#{rand(99)}" }
   end
 end
--- spec/factories/test_reports.rb
+++ spec/factories/test_reports.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
   factory :test_report do
+    token { SecureRandom.alphanumeric(7).upcase }
   end
 end
